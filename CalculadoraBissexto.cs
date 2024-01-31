df9717d62e7499dff6c78fd0ca52725763725015
Thalita Alencar <125590093+thallencar@users.noreply.github.com>
2024-01-30 23:20:42 -0300

    Add files via upload

---
+++ CalculadoraBissexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalculadoraBissexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu ano de nascimento.");
+            int birthday = int.Parse(Console.ReadLine());
+
+            if(birthday%4 == 0 && birthday%100 != 0 )
+            {
+                Console.WriteLine("Você nasceu em um ano bissexto.");
+            } else
+            {
+                Console.WriteLine("Voce não nasceu em um ano bissexto.");
+            }
+        }
+    }
+}
